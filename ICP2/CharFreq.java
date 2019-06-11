169af400e18a2ca81c6c24b6517a47783a47ee58
Abilash7 <46832142+Abilash7@users.noreply.github.com>
2019-06-11 14:33:06 -0500

    Update CharFreq.java

--- ICP2/CharFreq.java
+++ ICP2/CharFreq.java
@@ -1,1 +1,68 @@
+import java.io.IOException;
+import java.util.StringTokenizer;
 
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.*;
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.Reducer.Context;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+public class CharFreq {
+
+  public static class TokenizerMapper
+       extends Mapper<Object, Text, Text, IntWritable>{
+      private Text letter=new Text();
+      private final static IntWritable one = new IntWritable(1);
+
+
+    public void map(Object key, Text value, Context context
+                    ) throws IOException, InterruptedException {
+        StringTokenizer itr = new StringTokenizer(value.toString());
+        while (itr.hasMoreTokens())
+        {
+            String str = itr.nextToken().replaceAll("[^a-zA-Z]", "").toLowerCase();
+            for(int i = 0; i<str.length(); i++)
+            {
+                letter.set(new byte[] {(byte)str.charAt(i)});
+                context.write(letter, one);
+            }
+        }
+      
+    }
+  }
+
+  public static class IntSumReducer
+       extends Reducer<Text,IntWritable,Text,IntWritable> {
+      public void reduce(Text key, Iterable<IntWritable> values,
+              Context context
+              ) throws IOException, InterruptedException {
+    int sum=0;
+    for(IntWritable value:values){
+        if (key.toString()!=" "){
+        sum+=value.get();}
+    }
+      
+     context.write(key, new IntWritable(sum));
+        
+      }
+  }
+      
+
+  public static void main(String[] args) throws Exception {
+    Configuration conf = new Configuration();
+    Job job = Job.getInstance(conf, "word count");
+    job.setJarByClass(WordCount.class);
+    job.setMapperClass(TokenizerMapper.class);
+    job.setCombinerClass(IntSumReducer.class);
+    job.setReducerClass(IntSumReducer.class);
+    job.setOutputKeyClass(Text.class);
+    job.setOutputValueClass(IntWritable.class);
+    FileInputFormat.addInputPath(job, new Path(args[0]));
+    FileOutputFormat.setOutputPath(job, new Path(args[1]));
+    System.exit(job.waitForCompletion(true) ? 0 : 1);
+  }
+}
